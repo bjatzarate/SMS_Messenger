48833d8cb5e9f5745521eaebc9992f4f873a3668
bjatzarate <zaratebillyjoelarlo@yahoo.com>
2017-07-03 00:02:50 +0800

    main program separated

---
+++ lib/SMS_Messenger/MainProgram.java
@@ -0,0 +1,43 @@
+package SMS_Messenger;
+
+import java.awt.Font;
+import java.util.Enumeration;
+
+import javax.swing.UIDefaults;
+import javax.swing.UIManager;
+import javax.swing.plaf.FontUIResource;
+
+public class MainProgram
+{
+	public static void initializeFontSize()
+	{
+		float multiplier = 1.25f;
+		UIDefaults defaults = UIManager.getDefaults();
+		Enumeration<Object> e = defaults.keys();
+		while (e.hasMoreElements())
+		{
+			Object key = e.nextElement();
+			Object value = defaults.get(key);
+			if ( value instanceof Font )
+			{
+				Font font = (Font) value;
+				int newSize = Math.round(font.getSize() * multiplier);
+				if ( value instanceof FontUIResource )
+				{
+					defaults.put(key, new FontUIResource(font.getName(), font.getStyle(), newSize));
+				}
+				else
+				{
+					defaults.put(key, new Font(font.getName(), font.getStyle(), newSize));
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		initializeFontSize();
+		new MainWindow();
+	}
+	
+}
